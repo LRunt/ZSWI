76f018e18001852f90c865e42ebad83753c4fa46
apolakov <alena.z.polakovicova@gmail.com>
2022-04-08 13:33:47 +0200

    dekompoziční úprava třídy - rozdělení kodu do více funkcí

--- ColumnFilter.py
+++ ColumnFilter.py
@@ -1,49 +1,69 @@
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtCore import Qt, QModelIndex
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QSlider, QLabel, QGridLayout, \
-    QMenuBar, QMainWindow, QAction, QTreeWidget, QTreeWidgetItem, QTableWidget, QLineEdit, QTextEdit
+from PyQt5.QtWidgets import QVBoxLayout, QWidget, QLabel, QLineEdit
 
 
 class ColumnFilter(QWidget):
-    """
-    This "window" is a QWidget. If it has no parent, it
-    will appear as a free-floating window as we want.
-    """
-
-    table = QtWidgets.QTableWidget
 
     def __init__(self, tb):
+        """
+        Konstruktor připravý globální instanční proměnné pro okno s filtrem
+        :param tb: tabulka ze které se vytváří filtr
+        """
         super().__init__()
 
+        self.table = tb
         self.listWithColumns = []
 
-        self.table = QtWidgets.QTableWidget()
-        self.table = tb
+        self.label = None
+        self.textBox = None
+        self.listView = None
+        self.model = None
+
+        self.buildWindow()
+
+
+    def buildWindow(self):
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.makeSearchingLabel())
+        layout.addWidget(self.makeSearchingTextBox())
+        layout.addWidget(self.makeListView())
+
+        self.setLayout(layout)
+
+
+
+    def makeSearchingLabel(self):
+        self.label = QLabel("Column filters")
+        return self.label
+
+    def makeSearchingTextBox(self):
+        self.textBox = QLineEdit(self)
+        self.textBox.textChanged.connect(self.computeText)
+        return self.textBox
+
+
+    def makeListView(self):
+        """
+        vytvoří listView a model
+        cyklus prochází všechny sloupce tabulky
+        pro každý sloupce je vytvořen listViewItem apřidá se do modelu
+        listView si pak setne model
+        propojí se mistView s funkcí listViewOnClickAction, která se volá při kliku do listView
+        :return: listView
+        """
+
+        self.listView = QtWidgets.QListView()
+        self.model = QStandardItemModel()
 
         tableColumnCount = self.table.columnCount()
 
-
-        layout = QVBoxLayout()
-        self.label = QLabel("Column filters")
-        layout.addWidget(self.label)
-
-        self.textbox = QLineEdit(self)
-
-        layout.addWidget(self.textbox)
-
-        self.textbox.textChanged.connect(self.computeText)
-
-        self.listView = QtWidgets.QListView()
-
-        self.model = QStandardItemModel()
-
-        counter = 0
-
         for i in range(tableColumnCount):
 
-            item = QStandardItem(self.table.horizontalHeaderItem(counter).text())
-            self.listWithColumns.append(self.table.horizontalHeaderItem(counter).text())
+            item = QStandardItem(self.table.horizontalHeaderItem(i).text())
+            self.listWithColumns.append(self.table.horizontalHeaderItem(i).text())
 
             if (self.table.isColumnHidden(i)):
                 check = Qt.Unchecked
@@ -51,28 +71,30 @@ class ColumnFilter(QWidget):
                 check = Qt.Checked
 
             item.setEditable(False)
-
-            item.setCheckState(check)
             item.setCheckable(True)
+            item.setCheckState(check)
+
             self.model.appendRow(item)
 
-            counter = counter + 1
-
-
         self.listView.setModel(self.model)
-        self.listView.clicked.connect(self.onClicke)
-
-        layout.addWidget(self.listView)
-
-        self.setLayout(layout)
+        self.listView.clicked.connect(self.listViewOnClickAction)
+        return self.listView
 
 
     def computeText(self, s):
 
+        """
+        Metoda se volá pokaždé když se změní obsah textboxu
+        :param s: string který je aktuálně v textBoxu
+        odstraní se celý model listView - lsiView je prázné
+        prochází se list se sloupci
+        pokud s (obsah textBoxu) je obsažen v názvu sloupce, vytvoříse item a přidá se do modelu
+        všechny tyto sloupce jsou sloupce, které jsou vyfiltrovány a nově zobrazeny v listboxu
+        """
+
         self.model.removeRows(0, self.model.rowCount())
 
         for index in range(len(self.listWithColumns)):
-
 
             if (s in str(self.listWithColumns[index])):
                 item = QStandardItem(self.listWithColumns[index])
@@ -80,14 +102,22 @@ class ColumnFilter(QWidget):
                     check = Qt.Unchecked
                 else:
                     check = Qt.Checked
+
                 item.setEditable(False)
-                item.setCheckState(check)
                 item.setCheckable(True)
+                item.setCheckState(check)
                 self.model.appendRow(item)
 
 
-    # @QtCore.pyqtSlot(QtWidgets.QTreeWidgetItem, int)
-    def onClicke(self, index):
+    def listViewOnClickAction(self, index):
+        """
+        Funkce reaguje na klik do listboxu
+        :param index: index prvku na který se kliklo
+        it je prvek na který jsem kliknul
+        zjistím jaký je jeho skutečný index - to je důležité, pokud použiji filtr a vyfiltruji si poslední sloupec
+        v listBoxu by měl index 0, ikdyž jeho skutečný index je např. 5
+        Když mám skutečný index skrývám a odkrývám podle potřeby
+        """
         number = index.row()
         model = self.listView.model()
         it = model.item(number)
@@ -98,7 +128,6 @@ class ColumnFilter(QWidget):
         for x in range(listCount):
             if (self.listWithColumns[x] == str(it.text())):
                 pureIndex = x
-
 
         if (self.table.isColumnHidden(pureIndex) == False):
             self.table.hideColumn(pureIndex)
