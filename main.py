5a2152d585b430f6b17861307a384e66b951a818
apolakov <alena.z.polakovicova@gmail.com>
2022-04-08 12:12:15 +0200

    Pridaný slider naväzujúci na textové pole, ktoré neumžňuje zadávanie čísel väčších než 100 alebo textu

--- main.py
+++ main.py
@@ -18,34 +18,103 @@ from DetailViewer import DetailViewer
 """
 Trida Slider se stara o vykreslovani slideru
 """
-class Slider(QWidget):
-    def __init__(self, parent=None):
-        super(Slider, self).__init__(parent)
+import sys
+from decimal import Decimal, ROUND_HALF_UP
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-        #nastaveni layoutu a defaultni hodnoty po nacteni
-        layout = QVBoxLayout()
-        self.l1 = QLabel("50")
-        self.l1.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.l1)
 
-        #nastaveni slideru
-        self.sl = QSlider(Qt.Horizontal)
-        self.sl.setMinimum(0)
-        self.sl.setMaximum(100)
-        self.sl.setValue(50)
-        self.sl.setTickPosition(QSlider.TicksBelow)
-        self.sl.setTickInterval(1)
+class DoublespinboxAndSlider(QtWidgets.QWidget):
+    def __init__(self, parent = None, minimum=0, maximum=100, step=1):
+        super(DoublespinboxAndSlider, self).__init__(parent)
 
-        #propojeni labelu a slideru
-        layout.addWidget(self.sl)
-        self.sl.valueChanged.connect(self.valuechange)
-        self.setLayout(layout)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
 
-    #promitnuti zmeny hodnoty
-    def valuechange(self):
-        size = self.sl.value()
-        #print(size)
-        self.l1.setText(str(size))
+        self.label = QtWidgets.QLabel('Label', self)
+        self.label.setSizePolicy(size_policy)
+
+        self.double_spinbox = QtWidgets.QDoubleSpinBox(self)
+        self.double_spinbox.setSizePolicy(size_policy)
+
+        self.slider = QtWidgets.QSlider(self)
+        self.slider.setOrientation(QtCore.Qt.Horizontal)
+        self.slider.setSizePolicy(size_policy)
+
+        self.double_spinbox.valueChanged.connect(self.double_spinbox_changed)
+        self.slider.valueChanged.connect(self.slider_changed)
+
+        self.vertical_layout = QtWidgets.QVBoxLayout(self)
+        self.vertical_layout.addWidget(self.label)
+        self.vertical_layout.addWidget(self.double_spinbox)
+        self.vertical_layout.addWidget(self.slider)
+
+        self.set_single_step(step)
+        self.slider.setMinimum(0)
+        self.set_minimum(minimum)
+        self.set_maximum(maximum)
+
+    def set_maximum(self, value):
+        self.double_spinbox.setMaximum(value)
+        self.set_slider_maximum()
+
+    def set_minimum(self, value):
+        self.double_spinbox.setMinimum(value)
+        self.set_slider_maximum()
+
+    def set_single_step(self, value):
+        self.double_spinbox.setSingleStep(value)
+        self.double_spinbox.setDecimals(len(str(value).split('.')[-1]))
+        self.set_slider_maximum()
+
+    def set_slider_maximum(self):
+        double_spinbox_range = self.double_spinbox.maximum() - self.double_spinbox.minimum()
+        slider_max = double_spinbox_range / self.double_spinbox.singleStep()
+        self.slider.setMaximum(int(slider_max))
+
+    def slider_changed(self, value):
+        value2 = self.round2(float(value) * self.double_spinbox.singleStep())
+        self.double_spinbox.setValue(value2)
+
+    def double_spinbox_changed(self, value):
+        value2 = int(self.round2(value / self.double_spinbox.singleStep()))
+        self.slider.setValue(value2)
+
+    def round2(self, value):
+        dicimals = str(self.double_spinbox.singleStep() / 10.0)
+        value2 = float(Decimal(str(value)).quantize(Decimal(dicimals), rounding=ROUND_HALF_UP))
+        return value2
+
+    def set_maximum(self, value):
+        self.double_spinbox.setMaximum(value)
+        self.set_slider_maximum()
+
+    def set_minimum(self, value):
+        self.double_spinbox.setMinimum(value)
+        self.set_slider_maximum()
+
+    def set_single_step(self, value):
+        self.double_spinbox.setSingleStep(value)
+        self.double_spinbox.setDecimals(len(str(value).split('.')[-1]))
+        self.set_slider_maximum()
+
+    def set_slider_maximum(self):
+        double_spinbox_range = self.double_spinbox.maximum() - self.double_spinbox.minimum()
+        slider_max = double_spinbox_range / self.double_spinbox.singleStep()
+        self.slider.setMaximum(int(slider_max))
+
+    def slider_changed(self, value):
+        value2 = self.round2(float(value) * self.double_spinbox.singleStep())
+        self.double_spinbox.setValue(value2)
+
+    def double_spinbox_changed(self, value):
+        value2 = int(self.round2(value / self.double_spinbox.singleStep()))
+        self.slider.setValue(value2)
+
+    def round2(self, value):
+        dicimals = str(self.double_spinbox.singleStep() / 10.0)
+        value2 = float(Decimal(str(value)).quantize(Decimal(dicimals), rounding=ROUND_HALF_UP))
+        return value2
+
+
 
 """
 Trida Menu se stara o vykreslovani menu - cele zkopirovano z netu
@@ -255,7 +324,7 @@ def window():
 
     #####
 
-    mySlider = Slider()
+    mySlider = DoublespinboxAndSlider()
     descriptionData = load_data('Data/dummy_texts.json.zip')
     detailViewer = DetailViewer(descriptionData)
     #pridani komponent do gridu
