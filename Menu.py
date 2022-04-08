ef5c05e9621f69c85c8df8ec5e860e2329b9f0ce
polac <polacek.pavel17@seznam.cz>
2022-04-08 13:56:32 +0200

    Úprava triggeru, který z menu otevře okno s filtrem

--- Menu.py
+++ Menu.py
@@ -1,19 +1,25 @@
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QSlider, QLabel, QGridLayout, \
     QMenuBar, QMainWindow, QTableView, QTableWidget, QStyledItemDelegate, QAction
 from PyQt5 import QtCore, QtWidgets
+
+from ColumnFilter import ColumnFilter
+
 
 class Menu(QWidget):
     """
     Trida Menu se stara o vykreslovani menu - cele zkopirovano z netu
     """
 
-    colFilter = None
+
 
     def __init__(self, tb):
         QWidget.__init__(self)
 
         self.table = QtWidgets.QTableWidget()
         self.table = tb
+
+        self.colFilter = None
 
         layout = QGridLayout()
         layout.setSpacing(0)
@@ -33,8 +39,8 @@ class Menu(QWidget):
         menubar.addMenu("Edit")
         menubar.addMenu("View")
         settingsFile = menubar.addMenu("Settings")
-        settingsFile.addAction("Column settings")
-        settingsFile.triggered[QAction].connect(self.processTrigger)
+        settingsFile.addAction("Column settings").triggered.connect(self.processTrigger)
+
         menubar.addMenu("Help")
 
     def processTrigger(self):
